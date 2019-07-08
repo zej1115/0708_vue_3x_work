d15b6fc6bd3162902bc72847f3fc4da2e517a2ef
hejuan <hejuan@sina.com>
2019-07-08 23:34:28 +0800

    init app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
+    <FooterGuide v-if="$route.meta.isShowFooter"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="text/ecmascript-6">
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default {
+    name: "App",
+    data () {
+      return {
+        msg: 'App Component'
+      }
+    },
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+    async mounted () {
+      // 分发actoin, 从后台获取数据到state中
+      this.$store.dispatch('getAddress')
+    },
+
+    components: {
+      FooterGuide
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus" scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
+
 import App from './App.vue'
+import router from './router'
+import store from './vuex/store'
+import Header from './components/Header/Header.vue'
+import Star from './components/Star/Star.vue'
 
-Vue.config.productionTip = false
+// Vue.prototype.$ajax = ajax
 
+
+// 注册全局组件
+Vue.component('Header', Header)
+Vue.component('Star', Star)
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  /* components: {
+    App
+  },
+  template: '<App/>' */
+  // render: createElement => createElement(App)  // <App/>
+  render: h => h(App),  // <App/>
+  router, // 配置路由器
+  store, // 配置vuex
+})
